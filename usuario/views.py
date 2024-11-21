27344a77481ecbf2b1c5d3064ae5f741429cc39b
ShuggarDV <diego.muara96@gmail.com>
2024-11-21 19:19:57 -0300

    Feat:nuevos filtros para usuario

--- usuario/views.py
+++ usuario/views.py
@@ -1,11 +1,13 @@
 import logging
+from datetime import datetime
+from django.utils.timezone import make_aware
 from rest_framework import viewsets, status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
-from .models import Usuario  # Asegúrate de que el modelo File esté importado
+from .models import Usuario  # Asegúrate de que el modelo Usuario esté importado
 from .serializers import UsuarioSerializer
 from .utils import upload_image_to_blob  # Importa la función para subir imágenes
-from datetime import date
+
 # Configurar logging
 logger = logging.getLogger(__name__)
 
@@ -30,21 +32,29 @@ class UsuarioViewSet(viewsets.ModelViewSet):
             logger.warning("No se proporcionó imagen, guardando usuario sin imagen.")
             serializer.save()  # Si no hay imagen, guarda el usuario sin la imagen
 
-@api_view(['GET'])
-def usuarios_registrados_hoy(request):
-    """
-    Endpoint para obtener los usuarios registrados el día de hoy.
-    """
-    try:
-        hoy = date.today()
-        usuarios_hoy = Usuario.objects.filter(created_at__date=hoy)  # Filtramos por la fecha de creación
-        serializer = UsuarioSerializer(usuarios_hoy, many=True)
-        logger.info("Usuarios registrados hoy cargados exitosamente.")
-        return Response(serializer.data, status=200)
-    except Exception as e:
-        logger.error(f"Error al cargar los usuarios registrados hoy: {str(e)}")
-        return Response({"error": "No se pudieron cargar los usuarios registrados hoy."}, status=500)
-    
+    @action(detail=False, methods=['get'], url_path='usuarios-registrados-hoy')
+    def get_usuarios_registrados_hoy(self, request):
+        """
+        Endpoint para obtener los usuarios registrados el día de hoy.
+        """
+        try:
+            # Obtener la fecha de hoy con timezone awareness
+            hoy = make_aware(datetime.now())
+            inicio_dia = hoy.replace(hour=0, minute=0, second=0, microsecond=0)
+            fin_dia = hoy.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+            # Filtrar los usuarios registrados hoy
+            usuarios_hoy = Usuario.objects.filter(created_at__gte=inicio_dia, created_at__lte=fin_dia)
+
+            # Serializar los datos
+            serializer = UsuarioSerializer(usuarios_hoy, many=True)
+            logger.info("Usuarios registrados hoy cargados exitosamente.")
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.error(f"Error al cargar los usuarios registrados hoy: {str(e)}")
+            return Response({"error": f"Error al cargar los usuarios registrados hoy: {str(e)}"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 @api_view(['POST'])
 def upload_image(request):
     if request.method == 'POST':
